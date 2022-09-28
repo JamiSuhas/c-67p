3bb7b2c7b957f09e0ad69fa84e0d8f2551f8ae40
Suhas <104132722+JamiSuhas@users.noreply.github.com>
2022-09-28 22:01:00 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
-};
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB1Lu0Bs4QpmFoYcNGWQllBt1_o2PZHoS8",
+    authDomain: "voting-app-6e1e2.firebaseapp.com",
+    projectId: "voting-app-6e1e2",
+    storageBucket: "voting-app-6e1e2.appspot.com",
+    messagingSenderId: "536078554196",
+    appId: "1:536078554196:web:8a97eeae4d7e4bedecc9c1"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
